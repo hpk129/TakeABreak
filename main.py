5df7a18f84f5f75e5129184357453014348e6952
Harmanpreet Kaur <hpkaur.129@gmail.com>
2024-11-26 05:13:41 +0530

    Added the files

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+        title = " 💧 Hydration Alert + Chill Break! 🛑",
+        message = "Hey, water champ! Time to sip some H2O and let your brain cool off.Stretch those legs, blink those eyes, and breathe. Hydrated minds think better—get back stronger! 🚀💦",
+        timeout = 10
+    )
